d947779449507711d606fb6e6235cac1fc7da3f0
IsmaelRP <37997622+IsmaelRP@users.noreply.github.com>
2024-03-03 22:33:10 +0100

    V.0.2
    Basic structure and functionality

---
+++ demo/src/main/java/com/example/demo/prices/application/obtainPrice/ObtainPriceUseCase.java
@@ -0,0 +1,22 @@
+package com.example.demo.prices.application.obtainPrice;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.prices.domain.model.PriceQuery;
+import com.example.demo.prices.domain.repository.PriceQueryRepository;
+
+@Service
+public class ObtainPriceUseCase {
+
+	@Autowired
+	private PriceQueryRepository priceQueryRepository;
+
+    public Optional<PriceQuery> findAllPosts(LocalDateTime application, int product_id, short subsidiary_id){
+        return this.priceQueryRepository.findByApplicationDate(application, product_id, subsidiary_id);
+    }
+
+}
